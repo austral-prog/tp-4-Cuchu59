7bddfa8aa75fb4889623449554133ba5bddc4d0d
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-04-12 09:48:10 -0400

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,10 +1,10 @@
 def leap_year():
     year = int(input('Ingrese un año: ')) 
     
-    if(year >= 1000 and (year % 400) == 0 ):
+    if(year >= 100 and (year % 400) == 0 ):
         print(f'El año {year} es bisiesto')
-    elif( year < 1000 and (year % 4) == 0):
+    elif( year < 100 and (year % 4) == 0):
         print(f'El año {year} es bisiesto')
     else:
-        print(f'El año no {year} es bisiesto')   
+        print(f'El año {year} no es bisiesto')     
   
