906e4c82f42a4f03f9cdec832d054e875fe64abe
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-04-12 09:32:06 -0400

    Update line.py

--- line.py
+++ line.py
@@ -1,6 +1,5 @@
 def line():
     import math
-    
     A = float(input('Ingrese el coeficiente A: '))
     B = float(input('Ingrese el coeficiente B: '))
     X1 = float(input('Ingrese el coeficiente X1: '))
@@ -14,16 +13,16 @@ def line():
 
     distance = math.dist(P1,P2)
 
-    print(f"""El coeficiente A de su ecuación de la recta es: {A}
-El coeficiente B de su ecuación de la recta es: {B}
-El coeficiente X1 de su ecuación de la recta es: {X1}
-El coeficiente X2 de su ecuación de la recta es: {X2}
-
-Para la siguiente ecuación:
-        Y = {A}x + {B}
-
-Dados los siguientes puntos:
-        P1 ({X1}, {Y1})
-        P2 ({X2}, {Y2})
-
-La distancia entre ellos es: {distance}""")
+    print(f'El coeficiente A de su ecuación de la recta es: {A}')
+    print(f'El coeficiente B de su ecuación de la recta es: {B}')
+    print(f'El coeficiente X1 de su ecuación de la recta es: {X1}')
+    print(f'El coeficiente X2 de su ecuación de la recta es: {X2}')
+    print()
+    print('Para la siguiente ecuación:')
+    print('\tY = {A}x + {B}')
+    print()
+    print('Dados los siguientes puntos:')
+    print(f'\tP1 ({P1[0]}, {P1[1]})')
+    print(f'\tP2 ({P2[0]}, {P2[1]})')
+    print()
+    print(f'La distancia entre ellos es: {distance}')
